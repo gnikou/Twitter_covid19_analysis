e1d14e6a4274c8da2ffc1705c24fc0ee02300ade
gnikou <georgenikou@yahoo.gr>
2022-05-02 10:56:52 +0300

    .

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 import json
-import numpy as np
-import pandas as pd
 import utils
-import torch, time, sys, re
+import re
 from collections import defaultdict
+
 from os import listdir
 from os.path import isfile, join
 import ast
 
 
-
-# tokenizer = AutoTokenizer.from_pretrained("vicgalle/xlm-roberta-large-xnli-anli")
-# model = AutoModelForSequenceClassification.from_pretrained("vicgalle/xlm-roberta-large-xnli-anli")
+tokenizer = AutoTokenizer.from_pretrained("vicgalle/xlm-roberta-large-xnli-anli")
+model = AutoModelForSequenceClassification.from_pretrained("vicgalle/xlm-roberta-large-xnli-anli")
+classifier = pipeline("zero-shot-classification",
+                      model="vicgalle/xlm-roberta-large-xnli-anli", device=0)
 
 
 # extract text field of twitter object
@@ -20,8 +20,8 @@ def get_text(tweet):
     if "extended_tweet" in tweet.keys() and "full_text" in tweet["extended_tweet"].keys():
         # case of extended tweet object
         return tweet['extended_tweet']['full_text']
-    elif "retweeted_status" in tweet.keys() and "extended_tweet" in tweet["retweeted_status"].keys() and "full_text" in \
-            tweet["retweeted_status"]["extended_tweet"].keys():
+    elif "retweeted_status" in tweet.keys() and "extended_tweet" in tweet["retweeted_status"].keys() and "full_text" \
+            in tweet["retweeted_status"]["extended_tweet"].keys():
         # case of retweet object with extednded status
         return tweet['retweeted_status']['extended_tweet']['full_text']
     elif "retweeted_status" in tweet.keys() and "full_text" in tweet["retweeted_status"].keys():
@@ -57,7 +57,7 @@ def legit_length(text):
     text.replace("  ", " ")
     words = [word for word in text.split(" ") if len(word) >= 1 and word[0] != "@"]
     score = len(words) / len("".join(words)) if len("".join(words)) != 0 else 0.0
-    if score > 0.0 and score < 1.0:
+    if 0.0 < score < 1.0:
         return True
     return False
 
@@ -93,60 +93,84 @@ def remove_rt(text):
 
 def helper(tweets):
     ids = set()
-    # tweet_texts = defaultdict(lambda: "")
-    # tweet_multi = defaultdict(lambda: 1)
-    tweetText_list = []
+    tweet_texts = defaultdict(lambda: "")
+    tweet_multi = defaultdict(lambda: 1)
+    tweet_text_list = []
+
     for tweet in tweets:
+
         tweet_text = remove_rt(remove_url(get_text(tweet)))
-        print(tweet_text)
-        if tweet_text is None or len(
-                tweet_text) < 3 or " " not in tweet_text or "account is temporarily unavailable because it violates the Twitter Media Policy. Learn more." in tweet_text or "account has been withheld in " in tweet_text:
-            continue
-        ids.add(tweet["id"])
-        tweet_texts[tweet["id"]] = tweet_text
-        print(tweet_text)
-    #
-    # for tweet in tweets:
-    #     for text in tweet.find({"retweeted_status": {"$exists": True}}):
-    #
-    #         rt_text = remove_rt(remove_url(get_text(text)))
-    #         if rt_text is None or len(rt_text) < 3 or " " not in rt_text:
-    #             continue
-    #         seq_size = len(rt_text[:-5])
-    #
-    #         """check if text sequences is same with the original text"""
-    #         if (tweet["retweeted_status"]["id"] in ids) and rt_text[:-5] == tweet_texts[
-    #                                                                             tweet["retweeted_status"]["id"]][
-    #                                                                         :seq_size]:
-    #             """if text is same , we increase multiplier index"""
-    #             tweet_multi[tweet["retweeted_status"]["id"]] += 1
-    #         elif (tweet["retweeted_status"]["id"] not in ids):
-    #             """case when we dont have the original tweet text, so we keep id and text for analysis"""
-    #             ids.add(tweet["retweeted_status"]["id"])
-    #             tweet_texts[tweet["retweeted_status"]["id"]] = rt_text
-    #         elif "account is temporarily unavailable because it violates the Twitter Media Policy. Learn more." not in rt_text and "account has been withheld in " not in rt_text:
-    #             """case when we have the original tweet but text is different, need to store both for further analysis"""
-    #             print("{}\n{}\n{}\n----------------------".format(
-    #                 tweet_texts[tweet["retweeted_status"]["id"]],
-    #                 tweet["retweeted_status"]["id"], rt_text))
+
+        if "retweeted_status" not in tweet:
+            if tweet_text is None or len(
+                    tweet_text) < 3 or " " not in tweet_text or "account is temporarily unavailable because it violates the Twitter Media Policy. Learn more." in tweet_text or "account has been withheld in " in tweet_text:
+                continue
+            ids.add(tweet["id"])
+            tweet_texts[tweet["id"]] = tweet_text
+
+        elif "retweeted_status" in tweet:
+            rt_text = tweet_text
+            if rt_text is None or len(rt_text) < 3 or " " not in rt_text:
+                continue
+            seq_size = len(rt_text[:-5])
+
+            """check if text sequences is same with the original text"""
+            if (tweet["retweeted_status"]["id"] in ids) and rt_text[:-5] == tweet_texts[
+                                                                                tweet["retweeted_status"]["id"]][
+                                                                            :seq_size]:
+                """if text is same , we increase multiplier index"""
+                tweet_multi[tweet["retweeted_status"]["id"]] += 1
+            elif tweet["retweeted_status"]["id"] not in ids:
+                """case when we dont have the original tweet text, so we keep id and text for analysis"""
+                ids.add(tweet["retweeted_status"]["id"])
+                tweet_texts[tweet["retweeted_status"]["id"]] = rt_text
+            elif "account is temporarily unavailable because it violates the Twitter Media Policy. Learn more." not in rt_text and "account has been withheld in " not in rt_text:
+                """case when we have the original tweet but text is different, need to store both for further analysis"""
+                print("{}\n{}\n{}\n----------------------".format(
+                    tweet_texts[tweet["retweeted_status"]["id"]],
+                    tweet["retweeted_status"]["id"], rt_text))
 
     ids_list = []
     all_tweets = 0
     for tw_id in ids:
-        tweetText_list.append(tweet_texts[tw_id])
+        tweet_text_list.append(tweet_texts[tw_id])
         ids_list.append(tw_id)
         all_tweets += tweet_multi[tw_id]
+    print(f"Number of all tweets: {all_tweets}")
+    sent_classifier(tweet_text_list, tweet_multi, all_tweets, ids_list)
+
+
+def sent_classifier(tweet_text_list, tweet_multi, all_tweets, ids_list):
+    sentiment_labels = ['positive for COVID-19', 'negative for COVID-19']
+    sentiment = classifier(tweet_text_list, sentiment_labels, batch_size=16, gradient_accumulation_steps=4,
+                           gradient_checkpointing=True, fp16=True, optim="adafactor")
+
+    sent_scores = defaultdict(lambda: 0.0)
+
+    for item in range(len(sentiment)):
+        for ind in range(len(sentiment[item]['labels'])):
+            label = sentiment[item]['labels'][ind].replace(" ", "_")
+            sent_scores[label] += (sentiment[item]['scores'][ind] * tweet_multi[ids_list[item]])
+
+    header = ""
+    data = ""
+    for label in sentiment_labels:
+        label = label.replace(" ", "_")
+        header += "\t" + label
+        data += "\t{}".format((sent_scores[label] / all_tweets) * 100 if all_tweets != 0 else 0.0)
+
+    file_out = open("log.csv".replace(" ", "_"), "w+")
+    file_out.write("{}\n{}\n".format(header, data))
+    file_out.close()
 
 
 def main():
     file = 'ht_COVID19_95.json'
     with open(file, 'r') as inp:
-        tweets = []
         tweets_dict = []
         for line in inp:
             js = json.loads(line)
             tweets_dict.append(js)
-            tweets.append(line)
     helper(tweets_dict)
 
 
